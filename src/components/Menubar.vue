b64b3ed211985452f19fa0be7a6159dc85a179bf
Yang Ye <47234759+w233w@users.noreply.github.com>
2022-08-01 20:31:39 -0400

    Export menubar as vue

---
+++ src/components/Menubar.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav>
+		<h2 class="nav-header">Contents</h2>
+		<ul class="nav-ul">
+			<li><a href="./Introduction.html">Introduction</a></li>
+			<li><a href="./Hometown.html">Hometown</a></li>
+			<li><a href="./Gaming.html">Game life</a></li>
+		</ul>
+	</nav>
+</template>
