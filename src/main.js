18f0869822b4e0169a299a4862e44974936c1916
w233w <yylovemiku@outlook.com>
2022-08-02 00:18:16 -0400

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueSidebarMenu)
+app.mount("#app")
