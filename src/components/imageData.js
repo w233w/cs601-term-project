ddb91e0fda26a1a894b11db98625591a910dab55
Yang Ye <47234759+w233w@users.noreply.github.com>
2022-08-13 00:19:04 -0400

    Update imageData.js

--- src/components/imageData.js
+++ src/components/imageData.js
@@ -2,10 +2,10 @@ const images = require.context('../assets', true, /.jpg$/);
 
 const imageData = [
     {src: images('./2022.jpg'), msg: "2022, I'm graduated!"},
-    {src: images('./2013.jpg'), msg: "2022, In Beijing, China"},
-    {src: images('./2014.jpg'), msg: "2022, Trevel to Europe"},
-    {src: images('./2015.jpg'), msg: "2022, My old social media photo."},
-    {src: images('./2021.jpg'), msg: "2022, My ID photo on BU ID card."},
+    {src: images('./2013.jpg'), msg: "2014, In Beijing, China"},
+    {src: images('./2014.jpg'), msg: "2015, Trevel to Europe"},
+    {src: images('./2015.jpg'), msg: "2017, My old social media photo."},
+    {src: images('./2021.jpg'), msg: "2021, My ID photo on BU ID card."},
 ];
 
 export default imageData;
